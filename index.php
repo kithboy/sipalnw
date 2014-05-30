0029a358c76d9ac81954e1dad97b619bc7619803
kluck <nlikitlurcha@gmail.com>
2014-05-30 12:42:50 +0700

    add news

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"
       xmlns:og="http://ogp.me/ns#"
       xmlns:fb="http://www.facebook.com/2008/fbml">
@@ -8,6 +8,7 @@
 </head><body>
 <?php
 echo "Hello Sipa Angel! We're เทพ";
+echo "โดนปล่อยตัว";
 ?>
 </body>
 </html>
